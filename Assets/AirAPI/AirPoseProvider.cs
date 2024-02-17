b29d003ab122b23ce313372b15990a68ff1bb32a
Peng Cheng <peng.cheng@acyclic.ai>
2024-02-17 17:11:41 -0500

    add test
    tested on Windows
    
    should move AirAPI into a submodule for quick testing

--- Assets/AirAPI/AirPoseProvider.cs
+++ Assets/AirAPI/AirPoseProvider.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Runtime.InteropServices;
 using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
 using UnityEngine.Experimental.XR.Interaction;
-
+using UnityEngine.SpatialTracking;
 public class AirPoseProvider : BasePoseProvider
 {
-    
-    #if WINDOWS
+#if UNITY_EDITOR_WIN
     
     [DllImport("AirAPI_Windows", CallingConvention = CallingConvention.Cdecl)]
     public static extern int StartConnection();
@@ -21,32 +18,149 @@ public class AirPoseProvider : BasePoseProvider
 
     [DllImport("AirAPI_Windows", CallingConvention = CallingConvention.Cdecl)]
     public static extern IntPtr GetEuler();
-    #elif UNITY_EDITOR_LINUX
     
-    [DllImport("libar_drivers.so")]
+#elif UNITY_EDITOR_LINUX
+    
+    [DllImport("libar_drivers.so", CallingConvention = CallingConvention.Cdecl)]
     public static extern int StartConnection();
 
-    [DllImport("libar_drivers.so")]
+    [DllImport("libar_drivers.so", CallingConvention = CallingConvention.Cdecl)]
     public static extern IntPtr GetEuler();
-    #endif
+    
+#endif
 
-    // Start is called before the first frame update
-    void Start()
+    protected enum ConnectionStates
+    {
+        Disconnected = 0,
+        Offline = 1,
+        StandBy = 2,
+
+        Connected = 3
+    }
+
+    protected static ConnectionStates connectionState = ConnectionStates.Disconnected;
+
+    public bool IsConnecting()
+    {
+        return connectionState is ConnectionStates.Connected or ConnectionStates.StandBy;
+    }
+
+    public void TryConnect()
     {
         // Start the connection
-        StartConnection();
+        var code = StartConnection();
+        if (code == 1)
+        {
+            connectionState = ConnectionStates.StandBy;
+            Debug.Log("Glasses standing by");
+        }
+        else
+            Debug.LogError("Connection error: return code " + code);
+    }
 
+
+    public void TryDisconnect()
+    {
+        if (IsConnecting())
+        {
+            var code = StopConnection();
+            if (code == 1)
+            {
+                connectionState = ConnectionStates.Disconnected;
+                Debug.Log("Glass disconnected");
+            }
+            else
+            {
+                connectionState = ConnectionStates.Offline;
+                Debug.LogWarning("Glass disconnected with error: return code " + code);
+            }
+        }
+        else
+        {
+            Debug.Log("Glass not connected, no need to disconnect");
+        }
+    }
+
+    public float mouseSensitivity = 100.0f;
+
+    protected Quaternion FromGlasses = Quaternion.identity;
+
+    protected static Quaternion NO_READING_Q = Quaternion.Euler(90f, 0, 0);
+
+    protected Vector3 FromMouse_Euler = Vector3.zero;
+    protected Quaternion FromMouse = Quaternion.identity;
+
+    protected Vector3 FromZeroing_Euler = Vector3.zero;
+    protected Quaternion FromZeroing = Quaternion.identity;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        TryConnect();
+    }
+
+    private void OnDestroy()
+    {
+        TryDisconnect();
     }
 
     // Update Pose
-    public override bool TryGetPoseFromProvider(out Pose output)
+    public override PoseDataFlags GetPoseFromProvider(out Pose output)
     {
-        IntPtr ptr = GetEuler();
-        float[] arr = new float[3];
+        if (IsConnecting()) UpdateFromGlasses();
+
+        if (Input.GetMouseButton(1))
+        {
+            UpdateFromMouse();
+        }
+
+        var compound = FromMouse * FromZeroing * FromGlasses;
+
+        output = new Pose(new Vector3(0, 0, 0), compound);
+        return PoseDataFlags.Rotation;
+    }
+
+    private void UpdateFromGlasses()
+    {
+        var ptr = GetEuler();
+        var arr = new float[3];
         Marshal.Copy(ptr, arr, 0, 3);
 
-        Quaternion target = Quaternion.Euler(arr[1] + 90.0f, -arr[2], -arr[0] + 180.0f);
-        output = new Pose(new Vector3(0, 0, 0), target);
-        return true;
+        // Debug.Log("glasses input (Euler): " + arr);
+
+        var reading = Quaternion.Euler(-arr[1] + 90.0f, -arr[2], -arr[0]);
+        if (connectionState == ConnectionStates.StandBy)
+        {
+            if (!reading.Equals(NO_READING_Q))
+            {
+                connectionState = ConnectionStates.Connected;
+                Debug.Log("Glasses connected, start reading");
+                FromGlasses = reading;
+            }
+        }
+        else
+        {
+            FromGlasses = reading;
+        }
+    }
+
+    private void UpdateFromMouse()
+    {
+        var deltaY = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        var deltaX = -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        // Mouse & Unity XY axis are opposite
+
+        FromMouse_Euler += new Vector3(deltaX, deltaY, 0.0f);
+
+        // Debug.Log("mouse pressed:" + FromMouseXY);
+
+        FromMouse = Quaternion.Euler(-FromMouse_Euler);
+    }
+
+    public void ZeroY()
+    {
+        var fromGlassesY = (FromGlasses * FromMouse).eulerAngles.y;
+        FromZeroing_Euler.y = -fromGlassesY;
+        FromZeroing = Quaternion.Euler(FromZeroing_Euler);
     }
 }
--- Assets/AirAPI/Tests/Spike.cs
+++ Assets/AirAPI/Tests/Spike.cs
@@ -1,27 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
 using UnityEngine.TestTools;
 
-public class Spike
+namespace AirAPI.Tests
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void SpikeSimplePasses()
+    public class Spike
     {
-        // Use the Assert class to test conditions
-        
-        // int code = AirPoseProvider.StartConnection();
-    }
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SpikeSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            
+            // int code = AirAPI.AirPoseProvider.StartConnection();
+        }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator SpikeWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SpikeWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
     }
 }
