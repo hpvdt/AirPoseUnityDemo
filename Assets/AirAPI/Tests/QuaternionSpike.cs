117f75bb83ca27282b4e05e32152c510aae75c81
Peng Cheng <peng.cheng@acyclic.ai>
2025-07-01 16:27:05 -0400

    ignore QuaternionSpike.cs

--- Assets/AirAPI/Tests/QuaternionSpike.cs
+++ Assets/AirAPI/Tests/QuaternionSpike.cs
@@ -4,40 +4,44 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class QuaternionSpike
+namespace AirAPI.Tests
 {
-    private static List<Quaternion> anchors()
+    [Ignore("spike")]
+    public class QuaternionSpike
     {
-        // not used, only for debugging
-
-        var anchors = new List<Quaternion>();
-        anchors.Add(Quaternion.Euler(90f, 0f, 0f).normalized);
-        anchors.Add(Quaternion.Euler(0f, 90f, 0f).normalized);
-        anchors.Add(Quaternion.Euler(0f, 0f, 90f).normalized);
-        return anchors;
-    }
-
-    // A Test behaves as an ordinary method
-    [Test]
-    public void PrintAnchors()
-    {
-        var aa = anchors();
-        foreach (var a in aa) Debug.Log(a);
-        foreach (var a in aa)
+        private static List<Quaternion> Anchors()
         {
-            var inv = Quaternion.Inverse(a);
-            Debug.Log(inv);
-        }
-        // Use the Assert class to test conditions
-    }
+            // not used, only for debugging
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator SanityWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+            var anchors = new List<Quaternion>();
+            anchors.Add(Quaternion.Euler(90f, 0f, 0f).normalized);
+            anchors.Add(Quaternion.Euler(0f, 90f, 0f).normalized);
+            anchors.Add(Quaternion.Euler(0f, 0f, 90f).normalized);
+            return anchors;
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PrintAnchors()
+        {
+            var aa = Anchors();
+            foreach (var a in aa) Debug.Log(a);
+            foreach (var a in aa)
+            {
+                var inv = Quaternion.Inverse(a);
+                Debug.Log(inv);
+            }
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SanityWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
     }
 }
